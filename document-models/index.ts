66cc08fd17e03c78f717b476cd4ff525fca326c3
liberuum <liberom@proton.me>
2025-02-04 14:09:10 +0000

    add invoice doc model + editor

--- document-models/index.ts
+++ document-models/index.ts
@@ -3,3 +3,4 @@ export { module as AtlasFoundation } from "./atlas-foundation";
 export { module as Accounts } from "./accounts";
 export { module as AccountTransactions } from "./account-transactions";
 export { module as IntegrationSettings } from "./integration-settings";
+export { module as Invoice } from "./invoice";
