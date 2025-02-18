735828bac46fd1246e4f7cae0d7f830989016f8c
liberuum <liberom@proton.me>
2025-02-18 13:31:46 +0000

    change date format

--- editors/invoice/InvoicePDF.tsx
+++ editors/invoice/InvoicePDF.tsx
@@ -123,7 +123,7 @@ const formatDate = (dateString: string) => {
   if (!dateString) return "";
   const date = new Date(dateString);
   const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const month = date.toLocaleString('default', { month: 'short' }).toUpperCase();
   const year = date.getFullYear();
   return `${day}/${month}/${year}`;
 };
